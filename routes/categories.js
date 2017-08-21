21302720b66527e8759125ebeed770ab8f32ebb7
vasundharachimmad <vasudha.c8@gmail.com>
2017-08-21 11:22:15 -0700

    Truncate text and Categories view

--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,17 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:category', function(req, res, next) {
+		var posts = db.get('posts');
+
+	    posts.find({category: req.params.category},{},function(err, posts){
+		res.render('index',{
+  			'title': req.params.category,
+  			'posts': posts
+  		});
+	});
+	});
+
 router.get('/add', function(req, res, next) {
 		res.render('addcategory',{
   			'title': 'Add Category'
